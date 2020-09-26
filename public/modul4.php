30960616b1f1412a62f601fb733ddc0c5d693fe6
Gustav <Gustav.eikaas@gmail.com>
2020-09-26 12:35:06 +0200

    finished mod4

--- public/modul4.php
+++ public/modul4.php
@@ -86,32 +86,279 @@ echo("Gratulerer til <b>" . $winner ."</b> for å ha vunnet konkurransen");
 echo("<br>##############################");
  
 ?>
+ <!--
+Man kan enkelt vise brukeren hvilke felt som ikke er fylt ut i et skjema, med å bruke required i Input linjen.
+Jeg velger å gjøre det i PHP, i tilfelle oppgavebeskrivelsen var vag.
 
+  -->
 <br><h1>Oppgave 4</h1></br>
 <p> Alle feltene er obligatoriske </p>
-<form method="post" attribute="post" action="/modul3.php">
+<form method="post" attribute="post" action="/modul4.php">
     <p> Fornavn</p>
-<input type = "text" name = "fname" placeholder = "Fornavn" required> 
+<input type = "text" name = "fname" placeholder = "Fornavn" > <!-- Kunne bare satt inn required etter "Fornavn"  -->
     <p> Etternavn</p>
-<input type = "text" name = "lname" placeholder = "Etternavn" required> 
+<input type = "text" name = "lname" placeholder = "Etternavn" > 
     <p> Addresse </p>
-<input type="text" name = "address" placeholder = "Hundremeterskogen" required>
+<input type="text" name = "address" placeholder = "Hundremeterskogen" >
     <p>Telefon </p>
-<input type="number" name = "phone" placeholder = "123456789" required>
+<input type="number" name = "phone" placeholder = "123456789" >
     <p>E-post </p>
-<input type="email" name = "address" placeholder = "Ola.Nordmann@test.no" required>
+<input type="email" name = "email" placeholder = "Ola.Nordmann@test.no" >
     <p>Fødselsdato </p>
-<input type="date" name="bdate" required>
+<input type="date" name="bdate" >
     <p>Kjønn </p>
 <label for="gender"></label>
-<select id="gender" name="gender" required>
+<select id="gender" name="gender" >
   <option value="Mann">Mann</option>
   <option value="Kvinne">Kvinne</option>
   <option value="Annet">Annet</option>
 </select>
     <p>Interesser</p>
-<input type="text" name="interests" required>
+<input type="text" name="interests" >
     <p>Kursaktiviteter</p>
-<input type="text" name="Activities" required>
+<input type="text" name="activities" >
+    <p>Rolle </p>
+<input type="text" name="rolle" >
 <input type="submit" value="Send inn">
 </form>
+
+
+<?php
+if(empty($_POST['fname']) ){
+    echo("Navn er ikke fylt ut<br>");
+}else{
+    $fname_check = true;
+}
+if(empty($_POST['lname']) ){
+    echo("Etternavn er ikke fylt ut<br>");
+}else{
+    $lname_check = true;
+}
+if(empty($_POST['address']) ){
+    echo("Addresse er ikke fylt ut<br>");
+}else{
+    $address_check = true;
+}
+if(empty($_POST['phone']) ){
+    echo("Telefon er ikke fylt ut<br>");
+}else{
+    $phone_check = true;
+}
+if(empty($_POST['email']) ){
+    echo("E-mail er ikke fylt ut<br>");
+}else{
+    $email_check = true;
+}
+if(empty($_POST['bdate']) || ctype_digit ( $_POST['bdate']) ){
+    echo("Fødselsdato er ikke fylt ut<br>");
+}else{
+    $bdate_check = true;
+}
+if(empty($_POST['gender']) ){
+    echo("Kjønn er ikke fylt ut<br>");
+}else{
+    $gender_check = true;
+}
+if(empty($_POST['interests']) ){
+    echo("Interesser er ikke fylt ut<br>");
+}else{
+    $interests_check = true;
+}
+if(empty($_POST['activities']) ){
+    echo("Aktiviteter er ikke fylt ut<br>");
+}else{
+    $activities_check = true;
+}
+if(empty($_POST['rolle']) ){
+    echo("Rolle er ikke fylt ut<br>");
+}else{
+    $role_check = true;
+}
+#Sjekker om alle feltene er fylt ut
+if($fname_check && $lname_check && $address_check && $phone_check && $email_check && $bdate_check && $gender_check &&  $interests_check && $activities_check && $role_check){
+$fname_add = $_POST["fname"];
+$lname_add = $_POST["lname"];
+$address_add = $_POST["address"];
+$phone_add = $_POST["phone"];
+$email_add = $_POST["email"];
+$bdate_add = $_POST["bdate"];
+$gender_add = $_POST["gender"];
+$interests_add = $_POST["interests"];
+$activities_add = $_POST["activities"];
+$roles_add = $_POST["rolle"];
+
+$fname = array();
+$lname = array();
+$address = array();
+$phone = array();
+$email = array();
+$bdate = array();
+$gender = array();
+$interests = array();
+$activities = array();
+$roles = array();
+
+array_push($fname,$fname_add);
+array_push($lname,$lname_add);
+array_push($address,$address_add);
+array_push($phone,$phone_add);
+array_push($email,$email_add);
+array_push($bdate,$bdate_add);
+array_push($gender,$gender_add);
+array_push($interests,$interests_add);
+array_push($activities,$activities_add);
+array_push($roles,$roles_add);
+
+
+echo("Nytt medlem registrert");
+?>
+<table border="1">
+        <tr>
+            <th>Navn</th>
+            <th>Etternavn</th>
+            <th>Addresse</th>
+            <th>Telefon</th>
+            <th>E-post</th>
+            <th>Fødselsdato</th>
+            <th>Kjønn</th>
+            <th>Interesser</th>
+            <th>Kursaktiviteter</th>
+            <th>Rolle</th>
+        </tr>
+        <tr>
+            <td><?php echo $fname[0]; ?></td>
+            <td><?php echo $lname[0]; ?></td>
+            <td><?php echo $address[0]; ?></td>
+            <td><?php echo $phone[0]; ?></td>
+            <td><?php echo $email[0]; ?></td>
+            <td><?php echo $bdate[0]; ?></td>
+            <td><?php echo $gender[0]; ?></td>
+            <td><?php echo $interests[0]; ?></td>
+            <td><?php echo $activities[0]; ?></td>
+            <td><?php echo $roles[0]; ?></td>
+            
+        </tr>
+</table>
+<?php
+}
+?>
+<br><h1>Oppgave 5</h1></br>
+
+<?php
+$fname2[0] = "Arne";
+$lname2[0] = "Olufsen";
+$address2[0] = "Hundremeterskogen";
+$phone2[0] = 1881;
+$email2[0] = "Arne.olufsen@test.no";
+$bdate2[0] = "2020-01-01";
+$gender2[0] = "Mann";
+$interests2[0] = "Fisking";
+$activities2[0] = "Dansing";
+$roles2[0] = "Leder";
+
+?>
+
+<form method="post" attribute="post" action="/modul4.php">
+    <p> Fornavn</p>
+<input type = "text" name = "fname2" value = <?php echo($fname2[0]); ?>  >
+    <p> Etternavn</p>
+<input type = "text" name = "lname2" value = <?php echo($lname2[0]); ?> > 
+    <p> Addresse </p>
+<input type="text" name = "address2" value = <?php echo($address2[0]); ?> >
+    <p>Telefon </p>
+<input type="number" name = "phone2" value = <?php echo($phone2[0]); ?> >
+    <p>E-post </p>
+<input type="email" name = "email2" value = <?php echo($email2[0]); ?> >
+    <p>Fødselsdato </p>
+<input type="date" name="bdate2" value = <?php echo($bdate2[0]); ?>>
+    <p>Kjønn </p>
+<label for="gender"></label>
+<select id="gender" name="gender2" value = <?php echo($gender2[0]); ?>>
+  <option value="Mann">Mann</option>
+  <option value="Kvinne">Kvinne</option>
+  <option value="Annet">Annet</option>
+</select>
+    <p>Interesser</p>
+<input type="text" name="interests2" value = <?php echo($interests2[0]); ?> >
+    <p>Kursaktiviteter</p>
+<input type="text" name="activities2" value = <?php echo($activities2[0]); ?> >
+    <p>Rolle </p>
+<input type="text" name="role2" value = <?php echo($roles2[0]); ?> >
+<input type="submit" value="Endre verdier">
+</form>
+
+<?php
+
+
+if(!empty($_POST["fname2"]) && $fname2[0] != $_POST["fname2"]){
+    $fname2[0] = $_POST["fname2"];
+    echo("<br>Fornavn endret<br>");
+} else if(empty($_POST["fname2"])) {
+    echo "Du glemte å fylle inn fornavn";
+}
+if(!empty($_POST["lname2"])&& $lname2[0] != $_POST["lname2"]){
+    $lname2[0] = $_POST["lname2"];
+    echo("<br>Etternavn endret<br>");
+}
+if(!empty($_POST["address2"])&& $address2[0] != $_POST["address2"]){
+    $address2[0] = $_POST["address2"];
+    echo("<br>Addresse endret<br>");
+}
+if(!empty($_POST["phone2"])&& $phone2[0] != $_POST["phone2"]){
+    $phone2[0] = $_POST["phone2"];
+    echo("<br>Nummer endret<br>");
+}
+if(!empty($_POST["email2"])&& $email2[0] != $_POST["email2"]){
+    $email2[0] = $_POST["email2"];
+    echo("<br>Email endret<br>");
+}
+if(!empty($_POST["bdate2"])&& $bdate2[0] != $_POST["bdate2"]){
+    $bdate2[0] = $_POST["bdate2"];
+    echo("<br>Dato endret<br>");
+}
+if(!empty($_POST["gender2"])&&  $gender2[0] != $_POST["gender2"]){
+    $gender2[0] = $_POST["gender2"];
+    echo("<br>Kjønn endret<br>");
+}
+if(!empty($_POST["interests2"])&& $interests2[0] != $_POST["interests2"]){
+    $interests2[0] = $_POST["interests2"];
+    echo("<br>Interesse endret<br>");
+}
+if(!empty($_POST["activities2"])&&  $activities2[0] != $_POST["activities2"]){
+    $activities2[0] = $_POST["activities2"];
+    echo("<br>Aktivitet endret<br>");
+}
+if(!empty($_POST["role2"])&&  $roles2[0] != $_POST["role2"]){
+    $roles2[0] = $_POST["role2"];
+    echo("<br>Rolle endret<br>");
+}
+
+
+?>
+<table border="1">
+        <tr>
+            <th>Navn</th>
+            <th>Etternavn</th>
+            <th>Addresse</th>
+            <th>Telefon</th>
+            <th>E-post</th>
+            <th>Fødselsdato</th>
+            <th>Kjønn</th>
+            <th>Interesser</th>
+            <th>Kursaktiviteter</th>
+            <th>Rolle</th>
+        </tr>
+        <tr>
+            <td><?php echo $fname2[0]; ?></td>
+            <td><?php echo $lname2[0]; ?></td>
+            <td><?php echo $address2[0]; ?></td>
+            <td><?php echo $phone2[0]; ?></td>
+            <td><?php echo $email2[0]; ?></td>
+            <td><?php echo $bdate2[0]; ?></td>
+            <td><?php echo $gender2[0]; ?></td>
+            <td><?php echo $interests2[0]; ?></td>
+            <td><?php echo $activities2[0]; ?></td>
+            <td><?php echo $roles2[0]; ?></td>
+            
+        </tr>
+</table>
